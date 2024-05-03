7cf87f0e1fbeb7915700e824e2e2bed0e2058c8b
summon00163 <166971530+summon00163@users.noreply.github.com>
2024-05-03 17:30:38 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // JavaScript Document
+$.get("news.html",function(data){
+	$(".newscontent").load("news.html .main");
+});
+$.get("story/say.html",function(data){
+	$(".saycontent").load("story/say.html .saying");
+});
---
+++ story/story.js
@@ -0,0 +1,9 @@
+$.get("subgallery/activity.html",function(data){
+	$(".activitygallerycontent").load("subgallery/activity.html img");
+});
+$.get("subgallery/past.html",function(data){
+	$(".pastgallerycontent").load("subgallery/past.html img");
+});
+$.get("subgallery/activity.html",function(data){
+	$(".togethergallerycontent").load("subgallery/together.html img");
+});
