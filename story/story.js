e8aa5507b5573348f75f4f517f7be992a7599e73
summon00163 <166971530+summon00163@users.noreply.github.com>
2024-05-31 19:12:07 +0800

    Delete story/story.js

--- story/story.js
+++
@@ -1,9 +0,0 @@
-$.get("subgallery/activity.html",function(data){
-	$(".activitygallerycontent").load("subgallery/activity.html .main");
-});
-$.get("subgallery/past.html",function(data){
-	$(".pastgallerycontent").load("subgallery/past.html .main");
-});
-$.get("subgallery/activity.html",function(data){
-	$(".togethergallerycontent").load("subgallery/together.html .main");
-});
