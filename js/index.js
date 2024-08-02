37c116ec7ee2f93a1d51474679493d4de01607a3
summon00163 <166971530+summon00163@users.noreply.github.com>
2024-08-02 22:22:45 +0800

    更新 index.js

--- js/index.js
+++ js/index.js
@@ -14,13 +14,13 @@ var max=0;
 var typ=randint(1,3);
 var show=document.getElementById('show');
 if(typ===1){
-	max=27;
+	max=picCountA;
 }
 else if(typ===2){
-	max=42;
+	max=picCountP;
 }
 else{
-	max=6;
+	max=picCountT;
 }
 var picCount=randint(1,max);
 var load = document.createElement('img');
